526219d0d76eb0bd9b750fe0d8f2392d3716b56e
Martti Ylioja <mylioja@users.noreply.github.com>
2020-10-07 20:22:42 +0200

    Add regex to filter files by extension

--- main.cpp
+++ main.cpp
@@ -1,17 +1,34 @@
 #include <experimental/filesystem>
 #include <iostream>
+#include <regex>
 #include <string>
 
 namespace fs = std::experimental::filesystem;
 
 namespace {
 
-    int scan_files(std::string& root_dir)
+    const auto cpp_regex = std::regex(
+        "\\.(h|hpp|c|cc|cpp)",
+        std::regex::optimize | std::regex::extended
+    );
+
+
+    bool is_source_file(const fs::path& path)
+    {
+        std::string ext = path.extension().string();
+        return std::regex_match(ext, cpp_regex);
+    }
+
+
+    int scan_for_files(std::string& root_dir)
     {
         auto dir_iter = fs::recursive_directory_iterator(root_dir);
-        for (const auto& p: dir_iter)
+        for (const auto& entry: dir_iter)
         {
-            std::cout << p.path() << '\n';
+            if (fs::is_regular_file(entry.status()) && is_source_file(entry.path()))
+            {
+                std::cout << entry.path() << '\n';
+            }
         }
 
         return 0;
@@ -29,7 +46,7 @@ int main(int argc, char** argv)
 
     std::string root_dir = argv[1];
 
-    int err = scan_files(root_dir);
+    int err = scan_for_files(root_dir);
 
     return err;
 }
